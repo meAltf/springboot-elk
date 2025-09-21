186714ba3808356e3234b153cab2d7d187e3d7ed
meAltf <alataf.dce.cs1@gmail.com>
2025-09-21 11:50:52 +0530

    springBoot with ELK stack

---
+++ src/test/java/com/learn/springboot_elk/SpringbootElkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.springboot_elk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootElkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
